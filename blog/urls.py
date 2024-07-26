366f03dba8eeaaf67a7e65b2ffc55488f0d139eb
fmedinab <frankmedinas1997@gmail.com>
2024-07-25 21:53:40 -0600

    first commit

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views 
+app_name = 'posts'
+
+urlpatterns = [
+    path('',views.render_posts,name='posts'),
+    path("<int:post_id>", views.post_detail, name="post_detail"),
+]
