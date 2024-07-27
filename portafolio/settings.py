2710e4c2abe1d69fda2a396346f46410685dcf70
fmedinab <frankmedinas1997@gmail.com>
2024-07-26 23:40:52 -0600

    settings mudulel

--- portafolio/settings.py
+++ portafolio/settings.py
@@ -82,10 +82,10 @@ WSGI_APPLICATION = 'portafolio.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': 
+    
         #'ENGINE': 'django.db.backends.sqlite3',
        # 'NAME': BASE_DIR / 'db.sqlite3',
-       dj_database_url.config(
+     'default':dj_database_url.config(
            default='postgresql://postgres:postgres@localhost/postgres',
         conn_max_age=600
        )
