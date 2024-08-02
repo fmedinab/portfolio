06b97e6823758dd8aac44b7cb26b762003618753
fmedinab <frankmedinas1997@gmail.com>
2024-08-02 10:20:21 -0600

    setting update

--- portafolio/settings.py
+++ portafolio/settings.py
@@ -47,14 +47,14 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
+    'django.middleware.security.SecurityMiddleware','whitenoise.middleware.WhiteNoiseMiddleWare',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.WhiteNoiseMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'portafolio.urls'
