65094ec3aeb73b3921c4c4697b2e64f73429e871
fmedinab <frankmedinas1997@gmail.com>
2024-08-04 09:17:39 -0600

    update setting

--- portafolio/settings.py
+++ portafolio/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
        # 'NAME': BASE_DIR / 'db.sqlite3',
      'default':dj_database_url.config(
             
-           default='postgresql://portafolio_m08a_user:77H9tE75X2tPl7Frjbn5FF4ryD2vNrO6@dpg-cqhj9kg8fa8c73bt4nr0-a/portafolio_m08a',
+           default='postgresql://postgres:postgres@localhost/postgres',
         conn_max_age=600
        )
     
